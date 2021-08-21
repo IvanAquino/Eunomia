98ce93f6001d6639b999ad5060ff7f771c38e619
Ivan Aquino <ivnaqn@gmail.com>
2021-08-20 22:03:19 -0500

    Page header added

---
+++ resources/views/components/page-header.blade.php
@@ -0,0 +1,12 @@
+<section class="content-header">
+    <div class="row mb-2">
+        <div class="col-sm-6">
+            <h1>
+                {{ $title }}
+            </h1>
+        </div>
+        <div class="col-sm-6">
+            {!! $right !!}
+        </div>
+    </div>
+</section>
